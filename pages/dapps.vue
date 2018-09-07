8b49e993ebef8648671fddde60b29ce97645a1b5
lulifu <lulifu@gxb.io>
2018-09-07 21:14:09 +0800

    feat(DApps):add DApps page

--- pages/dapps.vue
+++ pages/dapps.vue
@@ -1,3 +1,53 @@
 <template>
-  <div>DApps</div>
+  <div class="dapps-page">
+    <section class="dapps section-padding">
+        <div class="container bottom-line padding-bottom-3">
+            <h2 class="gxc-border-left padding-left-w color-666">布洛克城</h2>
+            <div class="content row">
+                <div class="margin-top-4 col-lg-8">
+                   <p>布洛克城Dapp于2018年1月12日正式上线，是基于公信链—GXChain上开发的第一个大型个人移动端应用，它致力于解决个人数据管理和数据共享方面的问题。布洛克城让用户可以创建区块链身份，允许用户授权、管理个人数据，让数据所有权真正回归用户自身。其内生激励机制将鼓励用户多维度、更全面的完善身份标签。布洛克城既不缓存用户数据、又保护用户隐私安全，在用户授权的情况下，充分激活数据潜力，丰富数据应用场景。</p>
+                </div>
+                <div class="col-lg-4 text-center">
+                  <img src="../assets/img/dapps/bc_qr.png" alt="">
+                  <a href="https://docs.gxchain.org" target="_blank" class="btn btn-lg btn-theme">下载布洛克城体验</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section-padding">
+       <div class="container">
+        <ul class="list-unstyled">
+          <li class="media">
+            <div class="app-logo text-center mr-3">
+               <img class="w_7rem mb-3" src="../assets/img/dapps/bide.png" alt="Generic placeholder image">
+               <h6>币得</h6>
+            </div>
+            <div class="media-body align-self-center">
+              币得是一个基于GXChian的去中心化娱乐平台，通过“区块链+智能合约”技术解决信息不对称、信任难问题，确保公平、公正、公开的游戏环境，打造集幸运众筹、预测竞猜、数字竞拍、棋牌游戏、定制化合约的综合娱乐生态。
+            </div>
+          </li>
+          <li class="media my-4">
+             <div class="app-logo text-center mr-3">
+              <img class="w_7rem mb-3" src="../assets/img/dapps/wlm.png" alt="Generic placeholder image">
+                <h6>万利马</h6>
+            </div>
+            <div class="media-body align-self-center">
+              万利马是第一款基于Unitopia平台开发的区块链养成游戏应用。基于区块链的“公平、公开、不可篡改”的特性，打造全新的养成模式。游戏中每一匹马都是链上独一无二的存在，可增值、可收藏、可交易，具备丰厚的经济价值。
+            </div>
+          </li>
+          <li class="media">
+             <div class="app-logo text-center mr-3">
+               <img class="w_7rem mb-3" src="../assets/img/dapps/prophet.png" alt="Generic placeholder image">
+              <h6>预言家</h6>
+             </div>  
+            <div class="media-body align-self-center">
+             预言家Prophet是一个基于公信链的移动社交类预测市场平台。平台利用分布式公开账本增加预测市场的透明度，通过数字代币经济激励开发者在平台发布预测应用，为用户提供不同类型的预测事件，汇聚用户群体的智慧从而有效的预测未来。
+            </div>
+          </li>
+        </ul>
+       </div>
+    </section>
+  </div>
 </template>
+
+  
