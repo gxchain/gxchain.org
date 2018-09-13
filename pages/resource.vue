ca16249f566fe6603a143dc391b6c99ad622ff84
lulifu <lulifu@gxb.io>
2018-09-13 16:12:56 +0800

    style(resource): extract common style

--- pages/resource.vue
+++ pages/resource.vue
@@ -3,36 +3,37 @@
      <section class="client section-padding">
         <div class="container">
             <h2 class="gxc-border-left padding-left-w color-666">{{$t('resource.client.title')}}</h2>
-             <div class="content content-margin-top">
-              <div class="item gxs-wallet">
-                <div class="item-logo"><img src="~static/resource/gxs_wallet.png" alt=""></div>
-                <div class="item-des">
-                  <h4 class="color-333">{{$t('resource.client.gxsWallet.title')}}</h4>
-                  <div class="color-666">{{$t('resource.gxsWallet.des')}}</div>
+             <div class="content-margin-top">
+              <div class="item row">
+                <img src="~static/resource/gxs_wallet.png" alt="">
+                <div class="col-md-6">
+                  <h4 class="">{{$t('resource.client.gxsWallet.title')}}</h4>
+                  <div class="color-666">{{$t('resource.client.gxsWallet.des')}}</div>
                 </div>
-                <div class="item-download">
+                <div class="item-download col-md-4">
                    <a href="http://blockcity.gxb.io/download/" target="_blank" class="btn btn-theme">{{$t('resource.github.download')}}</a> 
                 </div>
               </div>
-               <div class="item pc-wallet">
-                <div class="item-logo"><img src="~static/resource/pc_wallet.png" alt=""></div>
-                <div class="item-des">
-                  <h4 class="color-333">{{$t('resource.client.pcWallet.title')}}</h4>
+              <div class="item row">
+                <img src="~static/resource/pc_wallet.png" alt="">
+                <div class="col-md-6">
+                  <h4 class="">{{$t('resource.client.pcWallet.title')}}</h4>
                   <div class="color-666">{{$t('resource.client.pcWallet.des')}}</div>
                 </div>
-                <div class="item-download">
+                <div class="item-download col-md-4">
                    <a href="hhttp://gxb-package.oss-cn-hangzhou.aliyuncs.com/gxb-light_2.0.180606(32bit).exe" target="_blank" class="btn btn-theme">{{$t('resource.github.win32')}}</a> 
                    <a href="http://gxb-package.oss-cn-hangzhou.aliyuncs.com/gxb-light_2.0.180606.exe" target="_blank" class="btn btn-theme">{{$t('resource.github.win64')}}</a> 
-                   <a href="http://gxb-package.oss-cn-hangzhou.aliyuncs.com/gxb-light_2.0.180606.dmg" target="_blank" class="btn btn-theme">{{$t('resource.github.mac')}}</a> 
+                   <a href="http://gxb-package.oss-cn-hangzhou.aliyuncs.com/gxb-light_2.0.180606.dmg" target="_blank" class="btn btn-theme">{{$t('resource.github.linux')}}</a> 
+                    <a href="http://gxb-package.oss-cn-hangzhou.aliyuncs.com/gxb-light_2.0.180606.dmg" target="_blank" class="btn btn-theme">{{$t('resource.github.mac')}}</a> 
                 </div>
               </div>
-               <div class="item agreement-ide">
-                <div class="item-logo"><img src="~static/resource/IDE.png" alt=""></div>
-                <div class="item-des">
-                  <h4 class="color-333">{{$t('resource.client.ide.title')}}</h4>
+               <div class="item row">
+                <img src="~static/resource/IDE.png" alt="">
+                <div class="col-md-6">
+                  <h4 class="">{{$t('resource.client.ide.title')}}</h4>
                   <div class="color-666">{{$t('resource.client.ide.des')}}</div>
                 </div>
-                <div class="item-download">
+                <div class="item-download col-md-4">
                    <a href="https://github.com/gxchain/gxchain-alpha/releases/tag/v0.0.3" target="_blank" class="btn btn-theme">{{$t('resource.github.download')}}</a> 
                 </div>
               </div>
@@ -42,7 +43,7 @@
      <section class="code-address section-padding" id="githubSource">
          <div class="container">
            <h2 class="gxc-border-left padding-left-w color-666">{{$t('resource.github.title')}}</h2>
-           <div class="content row content-margin-top">
+           <div class="row content-margin-top">
              <div class="col-md-4" v-for="(item,index) in codeAddrList" :key="index">
                <div class="item">
                   <h4 class="color-666">{{$t(('resource.github.codeAddrList.'+item.ikey)).title}}</h4>
@@ -99,7 +100,7 @@ export default {
           des: "主链交互Client,封装了账户注册，转账，区块查询等常用API",
           address: {
             java: {
-              path: "https://github.com/gxchain/baas-sdk-java"
+              path: "https://github.com/gxchain/gxclient-java"
             },
             node: {
               path: "https://github.com/gxchain/gxclient-node"
@@ -151,7 +152,7 @@ export default {
           des: "区块查询，交易查询，账户查询，资产查询",
           address: {
             leafFor: {
-              path: "https://block.gxb.io"
+              path: "https://github.com/gxchain/gxchain-explorer"
             }
           }
         }
@@ -161,70 +162,5 @@ export default {
 };
 </script>
 <style lang="less" scoped>
-.section {
-  padding-top: 50px;
-  padding-bottom: 30px;
-}
-.client {
-  .item {
-    display: flex;
-    flex-wrap: wrap;
-    align-items: center;
-    padding: 60px 0;
-    border-bottom: 2px solid #dbdbdb;
-  }
-  .agreement-ide {
-    border-bottom: none;
-  }
-  .item-logo {
-    margin-right: 30px;
-  }
-  .item-des {
-    width: 50%;
-  }
-  .item-download {
-    margin-left: 60px;
-    a {
-      margin: 0 10px;
-    }
-  }
-}
-.code-address {
-  .item {
-    padding: 36px;
-    background: #f5f6f9;
-    border-radius: 10px;
-    margin-bottom: 30px;
-    a {
-      color: #70bc55;
-      margin-right: 25px;
-    }
-    h6 {
-      min-height: 2.5rem;
-    }
-  }
-}
-@media (max-width: 1200px) {
-  .client {
-    .item-download {
-      margin-left: 0;
-    }
-  }
-}
-@media (max-width: 1080px) {
-  .client {
-    .item {
-      justify-content: center;
-      text-align: center;
-    }
-    .item-logo {
-      margin-right: 0;
-      margin-bottom: 20px;
-    }
-    .item-des {
-      width: 100%;
-      margin-bottom: 20px;
-    }
-  }
-}
+@import "../assets/css/resource.less";
 </style>
