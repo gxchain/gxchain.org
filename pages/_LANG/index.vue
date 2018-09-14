7ebff8b34fcc0db224db64a648e1f0bceab67f8e
lulifu <lulifu@gxb.io>
2018-09-14 17:19:32 +0800

    style(index):  modify the button style  of source's entrance

--- pages/_LANG/index.vue
+++ pages/_LANG/index.vue
@@ -70,7 +70,7 @@
             <div class="down-load">
               <p>{{$t('index.resource.txt1')}}</p><p>{{$t('index.resource.txt2')}}</p>
             </div>
-            <a href="https://github.com/gxchain" target="_blank" class="btn btn-lg btn-theme-lg">{{$t('index.resource.txt3')}} </a> 
+            <a href="https://github.com/gxchain" target="_blank" class="btn-resource">{{$t('index.resource.txt3')}} </a> 
           </div>
         </div>
     </section>
