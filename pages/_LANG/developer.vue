8b8b301ad84a14bc8ed12c5f4821fc4a646fc349
lulifu <lulifu@gxb.io>
2018-09-14 13:46:02 +0800

    style(developer): change the button style

--- pages/_LANG/developer.vue
+++ pages/_LANG/developer.vue
@@ -15,13 +15,13 @@
               <img class="item-bg" src="~static/developer/gxb.png" alt="">
               <h3>{{$t('developer.apply.title')}}</h3>
               <p>{{$t('developer.apply.des')}}</p>
-              <a href="http://blockcity.mikecrm.com/2SVDb67" target="_blank" class="btn btn-lg btn-theme">{{$t('developer.apply.btn')}}</a> 
+              <a href="http://blockcity.mikecrm.com/2SVDb67" target="_blank" class="btn btn-lg btn-theme-md">{{$t('developer.apply.btn')}}</a> 
            </div>
            <div class="item gitbub-entrance">
               <img class="item-bg" src="~static/developer/github.png" alt="">
               <h3>{{$t('developer.visit.title')}}</h3>
               <p>{{$t('developer.visit.des')}}</p>
-              <a href="https://github.com/gxchain" target="_blank" class="btn btn-lg btn-theme">{{$t('developer.visit.btn')}}</a> 
+              <a href="https://github.com/gxchain" target="_blank" class="btn btn-lg btn-theme-md">{{$t('developer.visit.btn')}}</a> 
            </div>
          </div>
        </div>
