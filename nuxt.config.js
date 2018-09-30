e8aeb85618aa48b9cb9e3ba77e18f117e3c416bc
lulifu <lulifu@gxb.io>
2018-09-30 17:22:44 +0800

    feat(nuxt.config):add foundation router

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,88 +1,88 @@
 const webpack = require('webpack')
 module.exports = {
-  /*
-   ** Headers of the page
-   */
-  head: {
-    title: 'gxchain.org',
-    link: [{
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: '/favicon.ico'
-      },
-      {
-        rel: 'stylesheet',
-        href: '//at.alicdn.com/t/font_835228_dxdsagyvm6o.css'
-      }
-    ],
-    meta: [{
-        charset: 'utf-8'
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
-      },
-      {
-        hid: 'description',
-        name: 'description',
-        content: 'home page of gxchain.org'
-      }
-    ]
-  },
-  /*
-   ** Customize the progress bar color
-   */
-  loading: {
-    color: '#6699ff'
-  },
-  /**
-   * modules
-   */
-  modules: [
-    'bootstrap-vue/nuxt',
-  ],
-  loader: [{
-    test: /\.less$/,
-    loaders: 'style-loader!css-loader!less-loader'
-  }],
-  /**
-   * plugins
-   */
-  plugins: [
-    '~/plugins/i18n.js'
-  ],
-  css: ['~assets/css/common.less'],
-  /*
-   ** Build configuration
-   */
-  build: {
     /*
-     ** Run ESLint on save
+     ** Headers of the page
      */
-    extend(config, {
-      isDev,
-      isClient
-    }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
+    head: {
+        title: 'gxchain.org',
+        link: [{
+                rel: 'icon',
+                type: 'image/x-icon',
+                href: '/favicon.ico'
+            },
+            {
+                rel: 'stylesheet',
+                href: '//at.alicdn.com/t/font_835228_dxdsagyvm6o.css'
+            }
+        ],
+        meta: [{
+                charset: 'utf-8'
+            },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1'
+            },
+            {
+                hid: 'description',
+                name: 'description',
+                content: 'home page of gxchain.org'
+            }
+        ]
     },
+    /*
+     ** Customize the progress bar color
+     */
+    loading: {
+        color: '#6699ff'
+    },
+    /**
+     * modules
+     */
+    modules: [
+        'bootstrap-vue/nuxt',
+    ],
+    loader: [{
+        test: /\.less$/,
+        loaders: 'style-loader!css-loader!less-loader'
+    }],
+    /**
+     * plugins
+     */
     plugins: [
-      new webpack.ProvidePlugin({
-        '$': 'jquery'
-      })
-    ]
-  },
-  router: {
-    middleware: 'i18n'
-  },
-  generate: {
-    routes: ['/', '/bounty', 'developer', 'resource', 'dapps', '/en', '/en/bounty', '/en/developer', '/en/resource', '/en/dapps']
-  }
+        '~/plugins/i18n.js'
+    ],
+    css: ['~assets/css/common.less'],
+    /*
+     ** Build configuration
+     */
+    build: {
+        /*
+         ** Run ESLint on save
+         */
+        extend(config, {
+            isDev,
+            isClient
+        }) {
+            if (isDev && isClient) {
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(js|vue)$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                })
+            }
+        },
+        plugins: [
+            new webpack.ProvidePlugin({
+                '$': 'jquery'
+            })
+        ]
+    },
+    router: {
+        middleware: 'i18n'
+    },
+    generate: {
+        routes: ['/', '/bounty', 'developer', 'resource', 'dapps', 'foundation', '/en', '/en/bounty', '/en/developer', '/en/resource', '/en/dapps', '/en/foundation']
+    }
 
 }
