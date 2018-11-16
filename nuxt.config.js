2f01f50bc1c808cf13ee59530a6ea3593e820bd9
lulifu <lulifu@gxb.io>
2018-11-16 19:37:35 +0800

    feat(config): add 'maximum-scale=1' to nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ module.exports = {
             },
             {
                 name: 'viewport',
-                content: 'width=device-width, initial-scale=1'
+                content: 'width=device-width, initial-scale=1, maximum-scale=1'
             },
             {
                 hid: 'description',
