6f35d3aa68731b60051d2d2745eea49ec1f72d70
lulifu <lulifu@gxb.io>
2019-03-21 19:44:08 +0800

    docs: Change the light wallet download address

--- pages/resource.vue
+++ pages/resource.vue
@@ -21,10 +21,9 @@
                   <div class="color-666 in-animate">{{$t('resource.client.pcWallet.des')}}</div>
                 </div>
                 <div class="item-download col-md-4 in-animate">
-                   <a href="https://gxb-package.oss-cn-hangzhou.aliyuncs.com/gxb-light_2.0.181106(32bit).exe" target="_blank" class="btn btn-theme-sm">{{$t('resource.github.win32')}}</a> 
-                   <a href="http://gxb-package.oss-cn-hangzhou.aliyuncs.com/gxb-light_2.0.181106.exe" target="_blank" class="btn btn-theme-sm">{{$t('resource.github.win64')}}</a> 
-                   <a href="http://gxb-package.oss-cn-hangzhou.aliyuncs.com/gxb-light_2.0.181106.dmg" target="_blank" class="btn btn-theme-sm">{{$t('resource.github.linux')}}</a> 
-                    <a href="http://gxb-package.oss-cn-hangzhou.aliyuncs.com/gxb-light_2.0.181106.dmg" target="_blank" class="btn btn-theme-sm">{{$t('resource.github.mac')}}</a> 
+                   <a href="https://github.com/gxchain/gxchain-light/releases/download/v1.3.0/gxb-light-setup-1.3.0.exe" class="btn btn-theme-sm">{{$t('resource.github.win')}}</a> 
+                   <a href="https://github.com/gxchain/gxchain-light/releases/download/v1.3.0/gxb-light_1.3.0_amd64.deb" target="_blank" class="btn btn-theme-sm">{{$t('resource.github.linux')}}</a> 
+                    <a href="https://github.com/gxchain/gxchain-light/releases/download/v1.3.0/gxb-light-1.3.0.dmg" target="_blank" class="btn btn-theme-sm">{{$t('resource.github.mac')}}</a> 
                 </div>
               </div>
                <div class="item row">
@@ -80,7 +79,7 @@ export default {
                     des: "GXChain核心模块",
                     address: {
                         leafFor: {
-                            path: "https://github.com/gxchain/gxchain-light/releases/tag/v1.3.0"
+                            path: "https://github.com/gxchain/gxchain-light"
                         }
                     }
                 },
