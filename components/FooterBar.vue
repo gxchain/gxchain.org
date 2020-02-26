7296ddba2b0ed5746d299c2f40c7ad5e7b06de64
junjie <250239016@qq.com>
2020-02-26 15:03:19 +0800

    chore: add telegram community and kakao  community

--- components/FooterBar.vue
+++ components/FooterBar.vue
@@ -13,8 +13,34 @@
             <a class="socail-icon iconfont icon-reddit" href="https://www.reddit.com/r/GXS/"></a>
             <a class="socail-icon iconfont icon-weibo" href="http://weibo.com/gongxinbao" target="_blank"></a>
             <a class="socail-icon iconfont icon-twitter3" href="https://twitter.com/gongxinbao" target="_blank"></a>
-            <a class="socail-icon iconfont icon-telegram" href="https://t.me/GXChain_international" target="_blank"></a>
+            <div class="socail-icon iconfont icon-telegram" >
+                <div class="item-wrap">
+                    <ul>
+                        <li><a href="https://t.me/GXChain_international" target="_blank">{{$t('footer.social.english_international')}} </a></li>
+                        <li><a href="https://t.me/GXChain_Rus" target="_blank">{{$t('footer.social.russian')}} </a></li>
+                        <li><a href="https://t.me/GXChainhispano" target="_blank">{{$t('footer.social.Spanish')}} </a></li>
+                        <li><a href="https://t.me/GXChainDeutsch" target="_blank">{{$t('footer.social.German')}} </a></li>
+                        <li><a href="https://t.me/GXChainJapanese" target="_blank">{{$t('footer.social.Japanese')}} </a></li>
+                        <li><a href="https://t.me/GXChainBrazil" target="_blank">{{$t('footer.social.Brazil')}} </a></li>
+                        <li><a href="https://t.me/GXChainIndia" target="_blank">{{$t('footer.social.Indian')}} </a></li>
+                        <li><a href="https://t.me/GXChainPhilippines" target="_blank" >{{$t('footer.social.Philippines')}} </a></li>
+                        <li><a href="https://t.me/GXChainVietNamese" target="_blank">{{$t('footer.social.Vietnamese')}} </a></li>
+                        <li><a href="https://t.me/GXChainAfrica" target="_blank">{{$t('footer.social.African')}} </a></li>
+                        <li><a href="https://t.me/GXChainTurkey" target="_blank">{{$t('footer.social.Turkish')}} </a></li>
+                        <li><a href="https://t.me/gxchainsrilanka" target="_blank">{{$t('footer.social.SriLanka')}} </a></li>
+                        <li><a href="https://t.me/GXChain_Indonesia" target="_blank">{{$t('footer.social.Indonesian')}} </a></li>
+                    </ul>
+                </div>
+            </div>
             <a class="socail-icon iconfont icon-facebookfacebook52" href="https://www.facebook.com/GXChain/" target="_blank"></a>
+            <div class="socail-icon iconfont icon-kakao">
+                <div class="item-wrap">
+                    <ul>
+                        <li><a href="https://open.kakao.com/o/g3JeEx0" target="_blank">{{$t('footer.social.kokor1')}} </a></li>
+                        <li><a href="https://open.kakao.com/o/gghzljsb" target="_blank">{{$t('footer.social.kokor2')}} </a></li>
+                    </ul>
+                </div>
+            </div>
           </div>
           <div class="social-list list-wrap" v-if="$i18n.locale === 'en'">
             <a class="wechat socail-icon iconfont icon-weixin-copy" href="javascript:void(0)">
@@ -27,8 +53,34 @@
             <a class="socail-icon iconfont icon-reddit" href="https://www.reddit.com/r/GXS/"></a>
             <a class="socail-icon iconfont icon-weibo" href="http://weibo.com/gongxinbao" target="_blank"></a>
             <a class="socail-icon iconfont icon-twitter3" href="https://twitter.com/GXChainGlobal" target="_blank"></a>
-            <a class="socail-icon iconfont icon-telegram" href="https://t.me/GXChain_international" target="_blank"></a>
+            <div class="socail-icon iconfont icon-telegram en-icon-item" target="_blank">
+                <div class="item-wrap">
+                    <ul>
+                        <li><a href="https://t.me/GXChain_international" target="_blank">{{$t('footer.social.english_international')}} </a></li>
+                        <li><a href="https://t.me/GXChain_Rus" target="_blank">{{$t('footer.social.russian')}} </a></li>
+                        <li><a href="https://t.me/GXChainhispano" target="_blank">{{$t('footer.social.Spanish')}} </a></li>
+                        <li><a href="https://t.me/GXChainDeutsch" target="_blank">{{$t('footer.social.German')}} </a></li>
+                        <li><a href="https://t.me/GXChainJapanese" target="_blank">{{$t('footer.social.Japanese')}} </a></li>
+                        <li><a href="https://t.me/GXChainBrazil" target="_blank">{{$t('footer.social.Brazil')}} </a></li>
+                        <li><a href="https://t.me/GXChainIndia" target="_blank">{{$t('footer.social.Indian')}} </a></li>
+                        <li><a href="https://t.me/GXChainPhilippines" target="_blank" >{{$t('footer.social.Philippines')}} </a></li>
+                        <li><a href="https://t.me/GXChainVietNamese" target="_blank">{{$t('footer.social.Vietnamese')}} </a></li>
+                        <li><a href="https://t.me/GXChainAfrica" target="_blank">{{$t('footer.social.African')}} </a></li>
+                        <li><a href="https://t.me/GXChainTurkey" target="_blank">{{$t('footer.social.Turkish')}} </a></li>
+                        <li><a href="https://t.me/gxchainsrilanka" target="_blank">{{$t('footer.social.SriLanka')}} </a></li>
+                        <li><a href="https://t.me/GXChain_Indonesia" target="_blank">{{$t('footer.social.Indonesian')}} </a></li>
+                    </ul>
+                </div>
+            </div>
             <a class="socail-icon iconfont icon-facebookfacebook52" href="https://www.facebook.com/gxchain.org" target="_blank"></a>
+            <div class="socail-icon iconfont icon-kakao en-icon-item-kakao" target="_blank">
+                <div class="item-wrap">
+                    <ul>
+                        <li><a href="https://open.kakao.com/o/g3JeEx0" target="_blank">{{$t('footer.social.kokor1')}} </a></li>
+                        <li><a href="https://open.kakao.com/o/gghzljsb" target="_blank">{{$t('footer.social.kokor2')}} </a></li>
+                    </ul>
+                </div>
+            </div>
           </div>
           <div class="other-list list-wrap">
             <a href="mailto:kefu@gxb.io">{{$t('footer.kefu')}}</a>
@@ -37,7 +89,7 @@
             <a v-show="$i18n.locale === 'zh'" target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33010602008310">浙公网安备 33010602008310号</a>
             <a v-show="$i18n.locale === 'zh'" href="http://www.miitbeian.gov.cn/">浙ICP备16042060号-1</a>
           </div>
-          <p class="copyright">© 2018-2019 GXChain Foundation Ltd.</p>
+          <p class="copyright">© 2018-2020 GXChain Foundation Ltd.</p>
         </div>
     </div>
 </template>
@@ -84,6 +136,130 @@
         }
       }
     }
+    .icon-telegram {
+        position: relative;
+        .item-wrap {
+            display: none;
+            position: absolute;
+            width: 450px;
+            top: -167px;
+            left: 50%;
+            font-size: 14px;
+            background-color: #434750;
+            margin-left: -225px;
+            ul {
+                text-align: left;
+                padding: 10px 10px 10px 30px;
+                li {
+                    width: 50%;
+                    display: inline-block;
+                    a {
+                        position: relative;
+                    }
+                    a::before {
+                        position: absolute;
+                        width: 0px;
+                        height: 0px;
+                        content: "";
+                        left: -19px;
+                        top: 2px;
+                        border-top: 5px solid transparent;
+                        border-bottom: 5px solid transparent;
+                        border-left: 12px solid #90a4ae;
+                        border-right: 12px solid transparent;
+                    }
+                }
+            }
+            &::before{
+                width: 0px;
+                height: 0px;
+                content: "";
+                position: absolute;
+                right: 47.5%;
+                top: 167px;
+                border-top: 7px solid #434750;
+                border-bottom: 7px solid transparent;
+                border-left: 12px solid transparent;
+                border-right: 12px solid transparent;
+            }
+        }
+        &:hover {
+            .item-wrap {
+                display: block;
+                a{
+                    color:#fff;
+                }
+            }
+        }
+    }
+    .en-icon-item{
+        .item-wrap {
+            width: 650px;
+            margin-left: -329px;
+        }
+    }
+    .icon-kakao {
+        position: relative;
+        .item-wrap {
+            display: none;
+            position: absolute;
+            width: 200px;
+            top: -60px;
+            left: 50%;
+            font-size: 14px;
+            background-color: #434750;
+            margin-left: -100px;
+            ul {
+                text-align: left;
+                padding: 10px 10px 10px 30px;
+                li {
+                    width: 100%;
+                    display: inline-block;
+                    a {
+                        position: relative;
+                    }
+                    a::before {
+                        position: absolute;
+                        width: 0px;
+                        height: 0px;
+                        content: "";
+                        left: -19px;
+                        top: 2px;
+                        border-top: 5px solid transparent;
+                        border-bottom: 5px solid transparent;
+                        border-left: 12px solid #90a4ae;
+                        border-right: 12px solid transparent;
+                    }
+                }
+            }
+            &::before{
+                width: 0px;
+                height: 0px;
+                content: "";
+                position: absolute;
+                right: 46.5%;
+                top: 62px;
+                border-top: 7px solid #434750;
+                border-bottom: 7px solid transparent;
+                border-left: 12px solid transparent;
+                border-right: 12px solid transparent;
+            }
+        }
+        &:hover {
+            .item-wrap {
+                display: block;
+                a{
+                    color:#fff;
+                }
+            }
+        }
+    }
+    .en-icon-item-kakao{
+        .item-wrap {
+            width: 280px;
+            margin-left: -143px;
+        }
+    }
   }
   .other-list {
     a {
@@ -91,6 +267,137 @@
       color: #666666;
     }
   }
+}
+@media (max-width: 414px) {
+    .footer {
+        .social-list{
+            .icon-telegram {
+                position: relative;
+                .item-wrap {
+                    display: none;
+                    position: absolute;
+                    width: 250px;
+                    top: -186px;
+                    left: 50%;
+                    font-size: 14px;
+                    background-color: #434750;
+                    margin-left: -118px;
+                    padding: 10px 0;
+                    ul {
+                        text-align: left;
+                        max-height:167px;
+                        overflow-y: scroll;
+                        padding: 0px 10px 0px 30px;
+                        li {
+                            width: 100%;
+                            display: inline-block;
+                            a {
+                                position: relative;
+                            }
+                            a::before {
+                                position: absolute;
+                                width: 0px;
+                                height: 0px;
+                                content: "";
+                                left: -19px;
+                                top: 2px;
+                                border-top: 5px solid transparent;
+                                border-bottom: 5px solid transparent;
+                                border-left: 12px solid #90a4ae;
+                                border-right: 12px solid transparent;
+                            }
+                        }
+                    }
+                    &::before{
+                        width: 0px;
+                        height: 0px;
+                        content: "";
+                        position: absolute;
+                        right: 47.5%;
+                        top: 186px;
+                        border-top: 7px solid #434750;
+                        border-bottom: 7px solid transparent;
+                        border-left: 12px solid transparent;
+                        border-right: 12px solid transparent;
+                    }
+                }
+                &:hover {
+                    .item-wrap {
+                        display: block;
+                        a{
+                            color:#fff;
+                        }
+                    }
+                }
+            }
+            .icon-kakao {
+                position: relative;
+                .item-wrap {
+                    display: none;
+                    position: absolute;
+                    width: 200px;
+                    top: -64px;
+                    left: 50%;
+                    font-size: 14px;
+                    background-color: #434750;
+                    margin-left: -100px;
+                    padding: 10px 0;
+                    ul {
+                        text-align: left;
+                        max-height:167px;
+                        overflow-y: scroll;
+                        padding: 0px 10px 0px 30px;
+                        li {
+                            width: 100%;
+                            display: inline-block;
+                            a {
+                                position: relative;
+                            }
+                            a::before {
+                                position: absolute;
+                                width: 0px;
+                                height: 0px;
+                                content: "";
+                                left: -19px;
+                                top: 2px;
+                                border-top: 5px solid transparent;
+                                border-bottom: 5px solid transparent;
+                                border-left: 12px solid #90a4ae;
+                                border-right: 12px solid transparent;
+                            }
+                        }
+                    }
+                    &::before{
+                        width: 0px;
+                        height: 0px;
+                        content: "";
+                        position: absolute;
+                        right: 46%;
+                        top: 61px;
+                        border-top: 7px solid #434750;
+                        border-bottom: 7px solid transparent;
+                        border-left: 12px solid transparent;
+                        border-right: 12px solid transparent;
+                    }
+                }
+                &:hover {
+                    .item-wrap {
+                        display: block;
+                        a{
+                            color:#fff;
+                        }
+                    }
+                }
+            }
+            .en-icon-item-kakao{
+                .item-wrap {
+                    width: 270px;
+                    margin-left: -137px;
+                }
+            }
+
+        }
+    }
 }
 </style>
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -4,76 +4,82 @@ module.exports = {
      ** Headers of the page
      */
     head: {
-        title: 'GXChain 可信数据的价值网络',
-        link: [{
-                rel: 'icon',
-                type: 'image/x-icon',
-                href: '/favicon.ico'
+        title: "GXChain 可信数据的价值网络",
+        link: [
+            {
+                rel: "icon",
+                type: "image/x-icon",
+                href: "/favicon.ico"
             },
             {
-                rel: 'stylesheet',
-                href: '//at.alicdn.com/t/font_835228_cv4vzh2wed.css'
+                rel: "stylesheet",
+                href: "//at.alicdn.com/t/font_835228_8cguzu07u8s.css"
             }
         ],
-        meta: [{
-                charset: 'utf-8'
+        meta: [
+            {
+                charset: "utf-8"
             },
             {
-                name: 'viewport',
-                content: 'width=device-width, initial-scale=1, maximum-scale=1'
+                name: "viewport",
+                content: "width=device-width, initial-scale=1, maximum-scale=1"
             },
             {
-                hid: 'description',
-                name: 'description',
-                content: '公信链GXChain是一条为全球数据经济服务的基础链，旨在打造可信数据的价值网络。'
+                hid: "description",
+                name: "description",
+                content:
+                    "公信链GXChain是一条为全球数据经济服务的基础链，旨在打造可信数据的价值网络。"
             },
             {
-                hid: 'keyswords',
-                name: 'keyswords',
-                content: 'GXChain官网，公信宝，区块链，GXChain白皮书，可信数据计算'
+                hid: "keyswords",
+                name: "keyswords",
+                content:
+                    "GXChain官网，公信宝，区块链，GXChain白皮书，可信数据计算"
             }
         ],
-        script: [{
-            type: 'application/ld+json',
-            src: '/js/schma.json',
-            head: true
-        }]
+        script: [
+            {
+                type: "application/ld+json",
+                src: "/js/schma.json",
+                head: true
+            }
+        ]
     },
     /*
      ** Customize the progress bar color
      */
     loading: {
-        color: '#6699ff'
+        color: "#6699ff"
     },
     /**
      * modules
      */
-    modules: [
-        'bootstrap-vue/nuxt',
+    modules: ["bootstrap-vue/nuxt"],
+    loader: [
+        {
+            test: /\.less$/,
+            loaders: "style-loader!css-loader!less-loader"
+        }
     ],
-    loader: [{
-        test: /\.less$/,
-        loaders: 'style-loader!css-loader!less-loader'
-    }],
     /**
      * plugins
      */
     plugins: [
-        '~/plugins/i18n.js',
+        "~/plugins/i18n.js",
         {
-            src: '~plugins/scrollreveal.js',
+            src: "~plugins/scrollreveal.js",
             ssr: false
         },
         {
-            src: '~/plugins/swiper.js',
+            src: "~/plugins/swiper.js",
             ssr: false
         },
         {
-            src: '~plugins/device.js',
+            src: "~plugins/device.js",
             ssr: false
         }
     ],
-    css: ['~assets/css/common.less', 'swiper/dist/css/swiper.css'],
+    css: ["~assets/css/common.less", "swiper/dist/css/swiper.css"],
     /*
      ** Build configuration
      */
@@ -81,30 +87,45 @@ module.exports = {
         /*
          ** Run ESLint on save
          */
-        extend(config, {
-            isDev,
-            isClient
-        }) {
+        extend(config, { isDev, isClient }) {
             if (isDev && isClient) {
                 config.module.rules.push({
-                    enforce: 'pre',
+                    enforce: "pre",
                     test: /\.(js|vue)$/,
-                    loader: 'eslint-loader',
+                    loader: "eslint-loader",
                     exclude: /(node_modules)/
-                })
+                });
             }
         },
         plugins: [
             new webpack.ProvidePlugin({
-                '$': 'jquery'
+                $: "jquery"
             })
         ]
     },
     router: {
-        middleware: ['i18n']
+        middleware: ["i18n"]
     },
     generate: {
-        routes: ['/', '/bounty', 'developer', 'resource', 'dapps', 'foundation', 'research', 'trustNodes', 'privacy', '/en', '/en/bounty', '/en/developer', '/en/resource', '/en/dapps', '/en/foundation', '/en/research', '/en/trustNodes', '/en/privacy']
+        routes: [
+            "/",
+            "/bounty",
+            "developer",
+            "resource",
+            "dapps",
+            "foundation",
+            "research",
+            "trustNodes",
+            "privacy",
+            "/en",
+            "/en/bounty",
+            "/en/developer",
+            "/en/resource",
+            "/en/dapps",
+            "/en/foundation",
+            "/en/research",
+            "/en/trustNodes",
+            "/en/privacy"
+        ]
     }
-
-}
+};
